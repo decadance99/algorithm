1d15ca68f8a202623d5b1914b08f7d4f3e3c9111
Eugene Hong <33304783+decadance99@users.noreply.github.com>
2020-11-26 21:07:03 +0900

    Update square.py

--- square.py
+++ square.py
@@ -1,19 +1,21 @@
-#2차원 배열 생성 (100x100) (모서리의 좌표들이 100 이하일 때를 가정하였으며,
-# 더 큰 정수가 들어올 때는 range 안의 수만 변경해 주면 됨)
+# Given x number of squares, calculate the total area. Squares may be overlapping.
+
+total_area = 0
+# 2D array of 0's (100x100) (assume: edge coordinates under 100)
 paper = [[0 for _ in range(101)] for _ in range(101)]
 
 x = int(input("Input the number of squares: "))
 
 for _ in range(x):
-    #제일 왼쪽 상단 모서리의 x좌표와 y좌표, 제일 오른쪽 하단 모서리의 x좌표와 y좌표만 추출
-    x1, y1, x2, y2 = map(int, input().split())
-    
-    #사각형 부분만 1로 바꾸기
-    for i in range(x1, x2):
-        for j in range(y1, y2):
-            paper[i][j] = 1
+  # get upper left coordinate(x, y) and lower right coordinate(x, y) of each sqaure
+  x1, y1, x2, y2 = map(int, input().split())
+  
+  # change the array from 0 to 1 where each square is located
+  for i in range(x1, x2):
+    for j in range(y1, y2):
+      paper[i][j] = 1
 
-answer = 0
 for row in paper:
-    answer += sum(row)
-print(answer)
+  total_area += sum(row)
+    
+print(total_area)
